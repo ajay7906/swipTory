b2badbfc66f80a101e807c0f9cb2f28bf86f18df
Ajay Kumar <aks969014@gmail.com>
2025-08-15 23:34:54 +0530

    done changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/userControllers");
+const verifyToken = require("../middleware/verifyToken")
 
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 // router.get()
+// check the token is valid or not  for the home page to show the login and logout button
+router.get('/check-token', verifyToken, (req, res) => {
+    res.json({ 
+        success: true,
+        user: req.user
+    });
+})
 
 module.exports = router;
